261c7585134e7c7c8f1780b0d3973082f97b9457
Your Name <you@example.com>
2021-02-18 22:29:29 -0800

    save

--- cli.py
+++ cli.py
@@ -1,6 +1,25 @@
 import sys
 import youtube
 import subtitles
+from pathlib import Path
+
+
+def download_subtitles(link,lang='ru'):
+    subtitle_file = youtube.download_subtitles(link,lang)
+    print('Downloaded', subtitle_file)
+    f = open(subtitle_file, "r")
+    lines = f.readlines()
+    f.close()
+    ret = subtitles.get_clear_text(lines)
+    ret.insert(0, subtitle_file)
+    ret.insert(1, link)
+    out_file = Path(subtitle_file)
+    out_file = out_file.with_suffix('.txt')
+    f = open(out_file, 'w')
+    f.writelines(ret)
+    f.close()
+    print("Saved subtitles in '%s'" % out_file)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -9,16 +28,8 @@ if __name__ == "__main__":
                                               'https://www.youtube.com/watch?v=_WC40T3FH88'))
         exit(1)
     try:
-        subtitle_file = youtube.download_subtitles(sys.argv[1])
-        print('Downloaded',subtitle_file)
-        f = open(subtitle_file, "r")
-        lines = f.readlines()
-        f.close()
-        ret = subtitles.get_clear_text(lines)
-        ret.insert(0,subtitle_file)
-        ret.insert(1,sys.argv[1])
-        for l in ret:
-            print(l)
+        download_subtitles(sys.argv[1],'ru')
+        download_subtitles(sys.argv[1],'en')
 
     except Exception as err:
         print(err)
